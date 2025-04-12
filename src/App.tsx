12dcf528a3f5ee61de1a8e4a0c605ee76be04c9a
Diksha <dikshadabhole78@gmail.com>
2025-04-12 20:51:05 +0530

    Update booking functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Cars from './pages/Cars';
@@ -8,17 +8,16 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename="/">
       <div className="min-h-screen bg-gray-50">
         <Navbar />
         <main className="container mx-auto px-4 py-8">
           <Routes>
+            <Route index element={<Home />} />
             <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Navigate to="/" replace />} />
             <Route path="/cars" element={<Cars />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
       </div>
